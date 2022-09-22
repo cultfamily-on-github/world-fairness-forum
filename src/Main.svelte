37623d6681c7f6c2bc04b29d5210018ed607d2a2
Michael <michael.spengler@sap.com>
2022-09-22 23:24:58 +0200

    first commit

---
+++ src/Main.svelte
@@ -0,0 +1,30 @@
+<main>
+	<div class="text-center bg-midnight text-tahiti">
+		<h1 class="text-center text-5xl">
+			Welcome to the World Fairness Forum.
+		</h1>
+
+		<h2>The power they took from the people will return to the people.</h2>
+
+		<iframe
+			width="100%"
+			height="600"
+			src="https://www.youtube.com/embed/J7GY1Xg6X20"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		/>
+
+		<a href="https://cultdao.io/manifesto.pdf">CULT</a> <br /><br /> is the
+		financial hub of a new, free society.
+		<p><br /><br /></p>
+		<a href="https://cultdao.io/rvlt.pdf">Revolt 2 Earn</a> <br /><br /> is
+		the council & support system.
+		<p><br /></p>
+		<ul />
+	</div>
+</main>
+
+<style>
+</style>
